93f608d1e6048dce0c32dba1a4a8cb0f2b674f3c
Luis Gomez <49451508+LuiDEVBubble@users.noreply.github.com>
2020-08-21 14:13:09 -0400

    .

--- theFuncs.js
+++ theFuncs.js
@@ -2,3 +2,8 @@ let makeInvisible = () =>
 {
     document.getElementById("uglyBoy").className = "hidden"; //esta funcion hace que el elemento con el id "uglyBoy" se desaparesca por medio de CSS
 }
+
+let reappear = () =>
+{
+    document.getElementById("uglyBoy").className = "reappear";
+}
